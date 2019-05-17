75a5fa7d81b816b09ce6c9028f4b53764396ea7e
quonn <qber83@gmail.com>
2019-05-17 08:54:05 -0700

    Completed exercise 2

--- drills.js
+++ drills.js
@@ -1,12 +1,42 @@
 const stack = require("./stack-n-queue");
 let StarTrek = new stack();
+
+// .1) Create a stack class
+
 main = () => {
     StarTrek.push("Kirk");
     StarTrek.push("Spock");
     StarTrek.push("McCoy");
     StarTrek.push("Scotty");
+    StarTrek.pop();
 }
 
 main();
 
-console.log(JSON.stringify(StarTrek));
+// console.log(JSON.stringify(StarTrek));
+
+// .2) Useful methods for a stack
+// Implement peek() and display() isEmpty()
+
+peek = () => {
+    return StarTrek.top;
+}
+
+isEmpty = () => {
+    if(StarTrek.top === null){
+        return "The stack is empty"
+    }else{
+        return "The stack is not empty"
+    }
+}
+
+display = () => {
+
+    if(StarTrek.top === null){
+        return "The stack is empty"
+    }else{        
+        return StarTrek.top
+    }
+}
+
+console.log(display());
