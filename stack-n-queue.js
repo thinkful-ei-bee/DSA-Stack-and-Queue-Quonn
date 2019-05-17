177f1e5a7ee2590fe43b1b9ad6e30ddff86ba82d
quonn <qber83@gmail.com>
2019-05-17 08:53:54 -0700

    added pop() to stack class

--- stack-n-queue.js
+++ stack-n-queue.js
@@ -25,6 +25,15 @@ class Stack {
         const node = new _Node(data, this.top);
         this.top = node;
     }
+
+    pop() {
+        /* In order to remove the top of the stack, you have to point
+           the pointer to the next item and that next item becomes the
+           top of the stack */
+        const node = this.top;
+        this.top = node.next;
+        return node.data;
+    }
 }
 
 const starTrek = new Stack();
