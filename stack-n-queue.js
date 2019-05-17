f3f5946bfdd13c13feb6dde145fead119ee4787c
quonn <qber83@gmail.com>
2019-05-17 08:38:58 -0700

    created stack and queue

---
+++ stack-n-queue.js
@@ -0,0 +1,34 @@
+class _Node {
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+    }
+
+    push (data){
+        /* If the stack is empty, then the node will be the
+           top of the stack */
+        if (this.top === null) {
+            this.top = new _Node(data, null);
+            return this.top;
+        }
+    
+        /* If the stack already has something, 
+           then create a new node,
+           add data to the new node, and
+           have the pointer point to the top */
+        const node = new _Node(data, this.top);
+        this.top = node;
+    }
+}
+
+const starTrek = new Stack();
+
+
+
+module.exports = Stack;
